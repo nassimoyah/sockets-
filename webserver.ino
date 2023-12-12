df0fed82922c6fa11e989fc3c561bcbb2ddfd961
nassimoyah <107773569+nassimoyah@users.noreply.github.com>
2023-12-13 00:29:50 +0100

    Create webserver.ino

---
+++ webserver.ino
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+#include <WiFi.h>
+
+char* ssid = "esp32"; 
+char* pass = "123456789i"; 
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, pass);
+  Serial.println("establishing connecting to " + String(ssid)); 
+
+while (WiFi.status() != WL_CONNECTED){
+  delay(1000);
+  Serial.println(".");
+}
+
+Serial.println("connected to wifi " + String(ssid));
+Serial.println(WiFi.localIP());
+
+}
+
+void loop() {
+  
+}
+
